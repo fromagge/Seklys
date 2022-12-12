fcecb579b08b782f7fa24aece9101166ac771195
Gabriel <ygabo@pm.me>
2022-12-12 07:43:41 +0000

    added a basic todo list with react hook form, chakra and zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FormControl, HStack, Input, Text, VStack } from '@chakra-ui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+
+
+const schemazos123 = z.object({
+  todo: z.string().min(1),
+})
+
+type Schema = z.infer<typeof schemazos123>;
+
 
 function App() {
+
+  // This is not ok!
+  const [todos, setTodos] = React.useState<Schema[]>([],);
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm<Schema>({
+    resolver: zodResolver(schemazos123),
+  })
+
+  const addToDo = (data : Schema) => {
+    setTodos(x => [...x,{...data}]);
+    reset();
+  };
+
+
+  const onKeyDownInput = (event: React.KeyboardEvent<HTMLInputElement> ) => 
+  {
+
+    if (event.key === 'Enter') {
+      handleSubmit(addToDo)    ;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack margin={10} justify="center">
+      <form onSubmit={handleSubmit(addToDo)}>
+        <FormControl>
+
+          <Input id="todo" placeholder='Write your next best idea' onKeyDown={onKeyDownInput}
+            {...register("todo", {
+              required: true,
+            })}
+          />
+        </FormControl>
+
+      </form>
+      <VStack>
+      {todos.map(x => (<Text key={x.todo}>{x.todo}</Text>))}
+      </VStack>
+    </VStack>
   );
 }
 
